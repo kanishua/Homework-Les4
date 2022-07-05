2ce667db361f9a8c6a3bf9275abbe19eccf183dc
Olya Osypchuk <kanishua.o@gmail.com>
2022-07-05 17:08:21 +0300

    Apdate Main body

--- Homework Les4/Program.cs
+++ Homework Les4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace Homework_Les4
 {
@@ -6,7 +6,7 @@ namespace Homework_Les4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
