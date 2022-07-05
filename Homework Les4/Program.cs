e6aaa019b9f4317d0db5319fbf270df1162b5db7
Olya Osypchuk <kanishua.o@gmail.com>
2022-07-05 17:01:12 +0300

    Add project files.

---
+++ Homework Les4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Les4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
